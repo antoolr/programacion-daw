0712b5347e6324d75d519a4f99531a0d1d571a97
antoolr <antoniolozanorodriguez13@gmail.com>
2025-09-24 14:21:28 +0200

    primera clase de fundamentos

--- fundamentos java/src/App.java
+++ fundamentos java/src/App.java
@@ -1,5 +1,42 @@
 public class App {
+    public static boolean logico=true;
+        final static double PI=3.14573;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       System.out.println("Hello, world!");
+        int edad=18;
+        double salario=1586.67;
+        String nombre="me llamo Antonio";
+
+        int cuenta= (1+2+3)/3;
+       int resultado=1/3;
+        System.out.println(resultado);
+        double resultado2=1/3.0;
+        
+
+        int a=(int)2.6;
+        System.out.println(a);
+
+        System.out.println(logico);
+        System.out.println(PI);
+
+        int b='b'; //Un caracter se guarda en uno entero en un ASCII
+                System.out.println(b);
+        char c=98;
+        System.out.print(b+" "+c);
+
     }
+
+            /**Programa para imprimir texto
+             * @author Antonio Lozano Rodríguez
+             * @param  texto a imprimir
+             * @version 1.0
+             */
+            
+            public static void funcion(){
+                boolean logico=false;
+        
+    }   
+
+
 }
