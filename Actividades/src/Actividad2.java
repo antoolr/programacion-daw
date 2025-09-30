dda709a21536a25a570a548833f5bd425514933a
antoolr <antoniolozanorodriguez13@gmail.com>
2025-09-30 13:30:08 +0200

    actividades y principios de bucle

---
+++ Actividades/src/Actividad2.java
@@ -0,0 +1,126 @@
+import java.util.Scanner;
+
+public class Actividad2 {
+    
+
+public static void main (String[] args) {
+
+    //Ejercicio 2
+    // Lector de notas 
+    Scanner teclado=new Scanner(System.in);
+    System.out.println("Introduce numero");
+    int nota = teclado.nextInt();
+
+     if(nota==1){
+             System.out.println("Suspenso");
+         }
+    else if(nota==2){
+             System.out.println("Suspenso");
+         }
+    else if(nota==3){
+             System.out.println("Suspenso");
+         }
+    else if(nota==4){
+             System.out.println("Suspenso");
+         }
+       else  if(nota==5){
+             System.out.println("Aprobado");
+         }
+        else if(nota==6){
+            System.out.println("Bien");
+        }
+      else if(nota==7){
+     System.out.println("Notable");
+      }
+     else if (nota==8){
+    System.out.println("Notable");
+     }
+     else if (nota==9){
+    System.out.println("Sobresaliente");
+     }
+     else if (nota==10){
+    System.out.println("Sobresaliente");
+     }
+     else {
+               System.out.println("Valor incorrecto");
+        }
+
+    //Ejercicio 1
+
+    //Resolución ecuaciones segundo grado
+    int a=0,b=0,c=0;
+    System.out.println("Introduce los valores a b c: ");
+    a=teclado.nextInt();
+    b=teclado.nextInt();
+    c=teclado.nextInt();
+
+    //Calcular el discriminante
+    double x1=0, x2=0;
+    double discriminante=Math.pow(b,2)-4*a*c;
+
+    if (discriminante <0){
+        System.out.println("No hay soluciones");
+    
+    }
+    else if (discriminante == 0){
+
+        System.out.println("La unica solucion es: "+x1);
+    
+    }
+    else {
+        x1=(-b+Math.sqrt(discriminante))/(2*a);
+        x2=(-b-Math.sqrt(discriminante))/(2*a);
+        System.out.println("La solucion 1 es "+x1);
+        System.out.println("La solucion 1 es "+x2);
+    }
+
+    //Ejercicio 3
+
+    //Lector de año bisiesto
+    System.out.println("Introduce el día: ");
+    int dia=teclado.nextInt();
+    System.out.println("Introduce el mes: ");
+    int mes=teclado.nextInt();
+    System.out.println("Introduce el año: ");
+    int anio=teclado.nextInt();
+
+    if(mes==1 || mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12){
+        if(dia>=1 && dia<=31){
+            System.out.println("Fecha correcta");
+        }
+        else {
+            System.out.println("Fecha incorrecta");
+        }
+    }
+    if(mes==4 || mes==6 || mes==9 || mes==11){
+        if(dia>=1 && dia<=30){
+            System.out.println("Fecha correcta");
+         }
+        }
+        else {
+            System.out.println("Fecha incorrecta");
+            }
+           
+        if (mes==2){ //Caso febrero
+            if (dia>1 && dia==28){
+                System.out.println("Fecha correcta");
+            } 
+        }
+        else if (dia==29){
+            if( (anio %4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) {
+            System.out.println("Fecha correcta");
+        }
+        else{
+            System.out.println("Fecha incorrecta");
+            }
+        }
+        else{
+            System.out.println("Fecha incorrecta");
+        }
+    
+
+     teclado.close();
+
+         
+    }
+}
--- fundamentos java/src/Condicional.java
+++ fundamentos java/src/Condicional.java
@@ -1,26 +1,97 @@
+import java.util.Scanner;
+
 public class Condicional {
     public static void main (String[] args) {
 
-        int user = 30;
-        if (user <=18) {
-            System.out.println("User es muy joven");
-        }
-        else if (user > 18 && user <40) {
-            System.out.println("User está entre 19 y 39 años");
-        }
-        else if (user == 45 || user == 50) {
-            System.out.println("User esta entre 45 y 50 años");
-        }
-        else {
-            System.out.println("User tiene 40 años");
-        }
-           int numero=6;
-            if (numero%2==0 && numero%3 == 0){
-                System.out.println("El numero "+numero+" Es divisible por 2 y por 3");
-             } 
-             else {
-            System.out.println("El numero "+numero+" no es divisible por 2 y 3");
-        }
+        // int user = 30;
+        // if (user <=18) {
+        //     System.out.println("User es muy joven");
+        // }
+        // else if (user > 18 && user <40) {
+        //     System.out.println("User está entre 19 y 39 años");
+        // }
+        // else if (user == 45 || user == 50) {
+        //     System.out.println("User esta entre 45 y 50 años");
+        // }
+        // else {
+        //     System.out.println("User tiene 40 años");
+        // }
+        //    int numero=6;
+        //     if (numero%2==0 && numero%3 == 0){
+        //         System.out.println("El numero "+numero+" Es divisible por 2 y por 3");
+        //      } 
+        //      else {
+        //     System.out.println("El numero "+numero+" no es divisible por 2 y 3");
+        // }
+
+        //leer numero
+        //identificar dependiendo del numero el mes que es
+        //si introduzco 4, resultado tiene que ser = Abril
+        // fin_si
+
+
+         Scanner teclado=new Scanner(System.in);
+         System.out.println("Introduce numero");
+         int numero2 = teclado.nextInt();
+
+    //     if(mes==1){
+    //         System.out.println("Enero");
+    //     }
+    //    else if(mes==2){
+    //         System.out.println("Febrero");
+    //     }
+    //    else if(mes==3){
+    //         System.out.println("Marzo");
+    //     }
+    //    else if(mes==4){
+    //         System.out.println("Abril");
+    //     }
+    //   else  if(mes==5){
+    //         System.out.println("Mayo");
+    //     }
+    //    else if(mes==6){
+    //         System.out.println("Junio");
+    //     }
+    //    else if(mes==7){
+    //         System.out.println("Julio");
+    //     }
+    //    else if(mes==8){
+    //         System.out.println("Agosto");
+    //     }
+    //    else if(mes==9){
+    //         System.out.println("Septiembre");
+    //     }
+    //    else if(mes==10){
+    //         System.out.println("Octubre");
+    //     }
+    //     else if(mes==11){
+    //         System.out.println("Noviembre");
+    //     }
+    //     else if(mes==12){
+    //         System.out.println("Diciembre");
+    //     }
+    //     else {
+    //         System.out.println("Valor incorrecto");
+    //     }
+
+            //Vamos a hacerlo todo con un switch
+            switch(numero2){
+                case 1: System.out.println("Enero");break;
+                case 2: System.out.println("Febrero");break;
+                case 3: System.out.println("Marzo");break;
+                case 4: System.out.println("Abril");break;
+                case 5: System.out.println("Mayo");break;
+                case 6: System.out.println("Junio");break;
+                case 7: System.out.println("Julio");break;
+                case 8: System.out.println("Agosto");break;
+                case 9: System.out.println("Septiembre");break;
+                case 10: System.out.println("Octubre");break;
+                case 11: System.out.println("Noviembre");break;
+                case 12: System.out.println("Diciembre");break;
+
+
+            }
+
     }
 }
 
