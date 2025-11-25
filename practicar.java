20b53d7c9330a7b0ed3bcad85523704a5b749824
antoolr <antoniolozanorodriguez13@gmail.com>
2025-11-25 09:10:20 +0100

    examen t3

--- practicar.java
+++ practicar.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class practicar {
-
     public static void main(String[] args) {
     int[] array = {3, 1, 5, 7, 31, 54, 75, 89, 80, 69 };
 
